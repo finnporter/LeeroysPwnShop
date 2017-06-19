1e732deafb74098ba3b19a1b12d7c14f4e676daf
finnporter <iamfinnporter@gmail.com>
2017-06-19 11:42:45 +0100

    displays products by lowest quantity on overview

--- app.rb
+++ app.rb
@@ -5,5 +5,6 @@ require_relative 'controllers/products_controller.rb'
 require_relative 'controllers/suppliers_controller.rb'
 
 get '/' do
+  @products = Product.all.sort_by { |product| product.quantity }
   erb(:index)
 end
