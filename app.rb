cbb5e8afab3006a13c66bb2e4442fccbf6c4ab3b
finnporter <iamfinnporter@gmail.com>
2017-06-19 11:58:21 +0100

    adds the 5 lowest quantity products to overview

--- app.rb
+++ app.rb
@@ -5,6 +5,6 @@ require_relative 'controllers/products_controller.rb'
 require_relative 'controllers/suppliers_controller.rb'
 
 get '/' do
-  @products = Product.all.sort_by { |product| product.quantity }
+  @products = Product.all.sort_by { |product| product.quantity }[0...5]
   erb(:index)
 end
